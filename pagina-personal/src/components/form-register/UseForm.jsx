418ca9c113eb58447346372c1096986acd3205a3
chuans <juaazn.05@gmail.com>
2024-07-27 11:50:47 +0200

    Data save in localStorage

--- pagina-personal/src/components/form-register/UseForm.jsx
+++ pagina-personal/src/components/form-register/UseForm.jsx
@@ -3,24 +3,23 @@ import { useState } from 'react'
 export default function () {
   const [data, setData] = useState({ name: '', email: '' })
 
-  const handleSubmit = () => {
-    return null
-  }
-
   const handleInputChange  = (event) => {
     setData({ ...data, [event.target.name]: event.target.value })
-    console.log(data)
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    window.localStorage.setItem(`user`, JSON.stringify(data))
   }
 
   return (
     <>
       <h2>Resgistrarse</h2>
       <form className='container_user_register_form' onSubmit={handleSubmit}>
-        <input type="text" onChange={handleInputChange } />
-        <input type="email" onChange={handleInputChange } />
+        <input type="text" name='name' onChange={handleInputChange } />
+        <input type="email" name='email' onChange={handleInputChange } />
         <input type="submit" value="Enviar" />
       </form>
     </>
   )
 }
-  
