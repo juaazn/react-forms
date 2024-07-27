db719fa08bd8cd294313477c635c8fde178eec09
chuans <juaazn.05@gmail.com>
2024-07-27 16:55:55 +0200

    React router

--- pagina-personal/src/main.jsx
+++ pagina-personal/src/main.jsx
@@ -1,28 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './routers/Home'
 import './index.css'
 import Contact from './routers/Contact'
 import About from './routers/About'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/contact',
-    element: <Contact />
-  },
-  {
-    path: '/about',
-    element: <About />
-  }
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
