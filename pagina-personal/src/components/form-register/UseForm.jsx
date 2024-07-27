e6fa2aa805ab94bfa9688e3314981bf0f3dec7b6
chuans <juaazn.05@gmail.com>
2024-07-27 17:44:25 +0200

    Completed exercise web personal

--- pagina-personal/src/components/form-register/UseForm.jsx
+++ pagina-personal/src/components/form-register/UseForm.jsx
@@ -1,25 +1,52 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function () {
+  const initialState = {
+    name: '',
+    email: '',
+   }
+   
   const [data, setData] = useState({ name: '', email: '' })
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+  const navigate = useNavigate()
 
   const handleInputChange  = (event) => {
+    if (data.name.length + 1 < 4) {
+      setMessage('Name must be at least 3 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+      
+    }
+
     setData({ ...data, [event.target.name]: event.target.value })
   }
   
   const handleSubmit = (event) => {
     event.preventDefault()
     window.localStorage.setItem(`user`, JSON.stringify(data))
+    clearState()
+    setTimeout(() => {
+      navigate('/about')
+    })
+  }
+
+  const clearState = () => {
+    setData({ ...initialState })
   }
 
   return (
-    <>
-      <h2>Resgistrarse</h2>
+    <section className='register'>
+      <h1>Resgistrarse</h1>
       <form className='container_user_register_form' onSubmit={handleSubmit}>
-        <input type="text" name='name' onChange={handleInputChange } />
-        <input type="email" name='email' onChange={handleInputChange } />
-        <input type="submit" value="Enviar" />
+        <input className='input__data' value={data.name} type="text" name='name' placeholder='Nombre Completo' onChange={handleInputChange } />
+        <input className='input__data' value={data.email} type="email" name='email' placeholder='name@gamil.com' onChange={handleInputChange } />
+        <input className='input_submit' type="submit" value="Enviar" disabled={btnDisabled} />
+        <p style={{'color': 'red'}}>{message}</p>
       </form>
-    </>
+    </section>
   )
 }
--- pagina-personal/src/routers/About.jsx
+++ pagina-personal/src/routers/About.jsx
@@ -1,9 +1,20 @@
 import Layout from '../Layout/Layout'
 
 export default function About () {
+  const data = window.localStorage.getItem('user')
+  const user = JSON.parse(data)
+  console.log(data)
+
+
   return (
     <Layout>
-      <h1>About</h1>
+      <article className='container'>
+        <h1>About</h1>
+        <section>
+          <h3>Bienvenido {user.name}</h3>
+          <p>Tu correo se ha resgistrado correctamente<strong> {user.email}</strong></p>
+        </section>
+      </article>
     </Layout>
   )
 }
