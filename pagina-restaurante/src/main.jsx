eb16f91aee2a407cd66518d79fc2d45ae6daee53
chuans <juaazn.05@gmail.com>
2024-07-30 19:38:39 +0200

    added Routes

--- pagina-restaurante/src/main.jsx
+++ pagina-restaurante/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import Booking from './router/Booking.jsx'
+import Home from './router/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Booking />}/>
+        <Route path='/home' element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
